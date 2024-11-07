f1d11bc46781ab5864e31ac752e9b069de437a4a
PaulHenton <paul.e.henton@gmail.com>
2024-11-07 13:28:22 +0100

    Lots of fixes

--- optimisation.py
+++ optimisation.py
@@ -6,40 +6,58 @@ def score(T, Y):
     return integrate.trapezoid(Pstar, x=T)
     #return integrate.trapezoid(Y[:,1], x=T)
 
-def times_to_injections(Temps_injections:np.array, C_tot:float, deltas=False):
+def times_to_injections(Temps_injections:np.array, C_i:float, deltas=False):
     if deltas:
-        return [(t, 1/C_tot) for t in Temps_injections.cumsum()]
+        return [(t, C_i) for t in Temps_injections.cumsum()]
     else:
-        return [(t, 1/C_tot) for t in Temps_injections]
+        return [(t, C_i) for t in Temps_injections]
 
 def func_to_optimise(Temps_injections:np.array, args, deltas=True):
     C_tot, P, Q, Qp, t_stop, parameters, step= args
-    injections = times_to_injections(Temps_injections, C_tot, deltas)
+    C_i = C_tot/len(Temps_injections)
+    injections = times_to_injections(Temps_injections, C_i, deltas)
     T, Y = run_simulation(P, Q, Qp, injections, t_stop, parameters, step)
     return score(T, Y)
 
-def find_optimal(n_inj, C_tot, P, Q, Qp, t_stop, maxfun, parameters=parameters, step=0.2):
-    delta_min = 1.5
+def find_optimal(n_inj, C_tot, P, Q, Qp, t_stop, Ns, parameters=parameters, step=0.2):
+    delta_min = 0.0
     bounds = tuple([(0, t_stop-delta_min*n_inj)] + [(delta_min, t_stop/n_inj)]*(n_inj-1))
     args = C_tot, P, Q, Qp, t_stop, parameters, step
-    return optimize.direct(func_to_optimise, bounds, args=(args,), maxfun=maxfun)
+    return optimize.brute(func_to_optimise, bounds, args=(args,), Ns=Ns, full_output=True)
 
 if __name__ == "__main__":
     import time as t
     _, P0, Q0, Qp0 = y0_PCV
-    n_inj = 2
+    n_inj = 3
     C_tot = 1
-    t_stop = 120
-    start = t.perf_counter()
-    Result = find_optimal(n_inj, C_tot, P0, Q0, Qp0, t_stop, maxfun=10)
-    
-    stop = t.perf_counter()
-    print("optimisation time", stop-start)
-    Temps_injections = Result.x   
-    print(Temps_injections)
+    t_stop = 70
+    step = 0.2
+    args = C_tot, P0, Q0, Qp0, t_stop, parameters, step
+    for n_inj in range(1, 2):
+        print("n :", n_inj)
+        start = t.perf_counter()
+        Temps_injections, s, grid, SGrid = find_optimal(n_inj, C_tot, P0, Q0, Qp0, t_stop, Ns=2000, step=0.02)
+        stop = t.perf_counter()
+        print("Score : ", s)
+        #Temps_injections = Temps_injections.cumsum()
+        injections = times_to_injections(Temps_injections, C_tot/n_inj, deltas=True)
+        T, Y = run_simulation(P0, Q0, Qp0, injections, 120)
+        plot_simulation(T, Y)
+        s = score(T, Y)
+        plt.title(str(s))
+        #s = func_to_optimise(Temps_injections, args)
+        print(Temps_injections)
+        print("optimisation time :", stop-start)
+        print("Score : ", s)
+
+
+
+
+plot_score_space = False
+if plot_score_space:
     from matplotlib import cm
-    X = np.linspace(0, 120, 20)
-    Y = np.linspace(0, 120, 20)
+    X = np.linspace(15, 35, 20)
+    Y = np.linspace(15, 35, 20)
     args = C_tot, P0, Q0, Qp0, t_stop, parameters, 0.2
     Z = np.array([[func_to_optimise(np.array([x, y]), args, deltas=False) for x in X] for y in Y])
     X, Y = np.meshgrid(X, Y)
@@ -50,4 +68,3 @@ if __name__ == "__main__":
     fig2.colorbar(surf)
 
     plt.show()
-
